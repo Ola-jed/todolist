b7ecd3a2bff1efad38a4f4bfdd3b7e679f882817
olabijed@gmail.com <olabijed@gmail.com>
2021-07-12 07:20:46 +0100

    Adding device name for auth

--- lib/api/auth_service.dart
+++ lib/api/auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:todolist/api/api_base.dart';
 import 'package:todolist/api/exceptions/response_retrieving_exception.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 /// Service that calls backend for auth management
 class AuthService extends ApiBase {
@@ -15,7 +16,7 @@ class AuthService extends ApiBase {
   /// - signupData : The serialized data with email name and password
   Future<String> makeSignup(Map signupData) async {
     try {
-      signupData['device_name'] = signupData['email'];
+      signupData['device_name'] = await _getDeviceIdentity();
       var result = await postUrl(Uri.parse(signupUrl), json.encode(signupData));
       var jsonResult = json.decode(result) as Map<String, String>;
       if (jsonResult['token'] == null) throw Exception('Signup failed');
@@ -32,7 +33,7 @@ class AuthService extends ApiBase {
   /// - signinData : The serialized data with email and password
   Future<String> makeSignin(Map signinData) async {
     try {
-      signinData['device_name'] = signinData['email'];
+      signinData['device_name'] = await _getDeviceIdentity();
       var result = await postUrl(Uri.parse(signinUrl), json.encode(signinData));
       var jsonResult = json.decode(result) as Map<String, String>;
       if (jsonResult['token'] == null) throw Exception('Signin failed');
@@ -53,5 +54,13 @@ class AuthService extends ApiBase {
     } on Exception {
       return false;
     }
+  }
+
+  /// Get the device identity for auth requests
+  Future<String> _getDeviceIdentity() async {
+    var deviceIdentity = '';
+    AndroidDeviceInfo info = await DeviceInfoPlugin().androidInfo;
+    deviceIdentity = "${info.device}-${info.id}";
+    return deviceIdentity;
   }
 }
