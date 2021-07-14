024dd82038e03a1fe94671966184210f4b3eff4b
olabijed@gmail.com <olabijed@gmail.com>
2021-07-14 21:37:50 +0100

    Taskscreen task part updated

--- lib/views/screens/task_screen.dart
+++ lib/views/screens/task_screen.dart
@@ -15,8 +15,83 @@ class TaskScreen extends StatefulWidget {
 class _TaskScreenState extends State<TaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.task.title),
+      ),
+      body: Column(
+        children:<Widget> [
+          Container(
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.only(top: 5,bottom: 5),
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.white,
+                  width: 3
+                )
+              )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 5,bottom: 5),
+                  child: Text(
+                    widget.task.title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      decoration: TextDecoration.underline
+                    )
+                  )
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 5,bottom: 5),
+                  child: Text(
+                    widget.task.description,
+                    style: const TextStyle(
+                      fontSize: 17
+                    )
+                  )
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 5,bottom: 5),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'Priority : ${widget.task.priority}',
+                        style: const TextStyle(
+                          fontSize: 17
+                        )
+                      ),
+                      Spacer(),
+                      Text(
+                        'Date limit : ${widget.task.dateLimit.toString().substring(0,10)}',
+                        style: const TextStyle(
+                          fontSize: 17
+                        )
+                      )
+                    ]
+                  )
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 5,bottom: 5),
+                  child: CheckboxListTile(
+                    onChanged: (value){},
+                    contentPadding: EdgeInsets.all(0),
+                    title: const Text('Finished ? '),
+                    value: widget.task.isFinished,
+                    activeColor: Colors.black,
+                  )
+                )
+              ]
+            )
+          ),
+        ]
+      )
     );
   }
 }
