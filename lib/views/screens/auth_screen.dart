4718160021661c77c14b638a2ba012ba037cdbe3
olabijed@gmail.com <olabijed@gmail.com>
2021-07-30 18:20:08 +0100

    Bug of home screen accessible without session fixed

--- lib/views/screens/auth_screen.dart
+++ lib/views/screens/auth_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todolist/views/forms/signin_form.dart';
 import 'package:todolist/views/forms/signup_form.dart';
 
 enum AuthType { Signup, Signin }
 
+/// Auth screen for signup and signin
+/// We don't want to go back when we are on this screen
 class AuthScreen extends StatelessWidget {
   final authType;
   const AuthScreen({@required this.authType,Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: authType == AuthType.Signin ? SigninForm() : SignupForm()
+    return WillPopScope(
+      child: Scaffold(
+        body: authType == AuthType.Signin ? SigninForm() : SignupForm()
+      ),
+      onWillPop: () async {
+        await SystemNavigator.pop();
+        return false;
+      }
     );
   }
 }
